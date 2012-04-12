7b36d1725aa2ec3d681332ec973555489cc518e6
eigenRedding <patrick.d.hull@gmail.com>
2012-04-11 18:49:34 -0700

    first pass adapting devil's datamapper

--- diablo/resource.py
+++ diablo/resource.py
@@ -11,17 +11,23 @@ try:
 except ImportError:
     import simplejson as json
 
+import xmlrpclib, yaml
+
 from twisted.internet import defer
 from twisted.web.server import NOT_DONE_YET
 from twisted.web import http
 from twisted.web.resource import Resource as ResourceBase
-from .http import HTTPError, Response
+from diablo.http import HTTPError, Response, InternalServerError
+import util, datamapper
+
 
 
 class Resource(ResourceBase):
     """ Base class for all resources of the REST API. """
 
     _log = None
+    default_mapper = None
+    mapper = None
 
     """ No child resources.
 
@@ -50,12 +56,14 @@ class Resource(ResourceBase):
         Dispatch the request to corresponding handler function based
         on the request method (get/put/post/...).
         """
+        #self.content_type = request.getHeader('content-type') or 'application/json'
 
         try:
             m = getattr(self, request.method.lower(), None)
             if m:
-                d = defer.maybeDeferred(m, request, *self.args, **self.kw)
-                d.addCallback(self._createResponse)
+                data = self._get_input_data(request)
+                d = defer.maybeDeferred(m, request, data, *self.args, **self.kw)
+                d.addCallback(self._createResponse, request)
                 d.addErrback(self._errorResponse)
                 d.addCallback(self._prepareResponse, request)
                 return NOT_DONE_YET
@@ -88,13 +96,33 @@ class Resource(ResourceBase):
         request.write(response.content)
         request.finish()
 
-    def _createResponse(self, data):
+    def _createResponse(self, data, request):
         """ Create successful Response object.
 
         todo: support for different formats
         """
-        content = json.dumps(data)
-        return Response(http.OK, content, {'Content-Type': 'application/json'})
+        res = datamapper.format(request, data, self)
+        if res.code is 0:
+          res.code = 200
+        return Response(res.code, res.content, res.headers)
+
+    def _get_input_data(self, request):
+        """ If there is data, parse it, otherwise return None. """
+        # only PUT and POST should provide data
+        if not self._is_data_method(request):
+            return None
+
+        content = [row for row in request.read()]
+        content = ''.join(content) if content else None
+        return self._parse_input_data(content, request) if content else None
+
+    def _parse_input_data(self, data, request):
+        """ Execute appropriate parser. """
+        return datamapper.parse(data, request, self)
+
+    def _is_data_method(self, request):
+        """ Return True, if request method is either PUT or POST """
+        return request.method.upper() in ('PUT', 'POST')
 
 
 #
---
+++ diablo/util.py
@@ -0,0 +1,114 @@
+#  -*- coding: utf-8 -*-
+# util.py ---
+#
+# Created: Fri Dec 30 23:27:52 2011 (+0200)
+# Author: Janne Kuuskeri
+#
+
+
+import re
+
+
+charset_pattern = re.compile('.*;\s*charset=(.*)')
+
+
+def camelcase_to_slash(name):
+    """ Converts CamelCase to camel/case
+
+    code ripped from http://stackoverflow.com/questions/1175208/does-the-python-standard-library-have-function-to-convert-camelcase-to-camel-cas
+    """
+
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1/\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1/\2', s1).lower()
+
+
+def strip_charset(content_type):
+    """ Strip charset from the content type string.
+
+    :param content_type: The Content-Type string (possibly with charset info)
+    :returns: The Content-Type string without the charset information
+    """
+
+    return content_type.split(';')[0]
+
+
+def extract_charset(content_type):
+    """ Extract charset info from content type.
+
+    E.g.  application/json;charset=utf-8  ->  utf-8
+
+    :param content_type: The Content-Type string (possibly with charset info)
+    :returns: The charset or ``None`` if not found.
+    """
+
+    match = charset_pattern.match(content_type)
+    return match.group(1) if match else None
+
+def get_charset(request):
+    """ Extract charset from the content type
+    """
+
+    content_type = request.META.get('CONTENT_TYPE', None)
+    if content_type:
+        return extract_charset(content_type) if content_type else None
+    else:
+        return None
+
+
+def parse_accept_header(accept):
+    """ Parse the Accept header
+
+    todo: memoize
+
+    :returns: list with pairs of (media_type, q_value), ordered by q
+    values.
+    """
+
+    def parse_media_range(accept_item):
+        """ Parse media range and subtype """
+
+        return accept_item.split('/', 1)
+
+    def comparator(a, b):
+        """ Compare accept items a and b """
+
+        # first compare q values
+        result = -cmp(a[2], b[2])
+        if result is not 0:
+            # q values differ, no need to compare media types
+            return result
+
+        # parse media types and compare them (asterisks are lower in precedence)
+        mtype_a, subtype_a = parse_media_range(a[0])
+        mtype_b, subtype_b = parse_media_range(b[0])
+        if mtype_a == '*' and subtype_a == '*':
+            return 1
+        if mtype_b == '*' and subtype_b == '*':
+            return -1
+        if subtype_a == '*':
+            return 1
+        if subtype_b == '*':
+            return -1
+        return 0
+
+    if not accept:
+        return []
+
+    result = []
+    for media_range in accept.split(","):
+        parts = media_range.split(";")
+        media_type = parts.pop(0).strip()
+        media_params = []
+        q = 1.0
+        for part in parts:
+            (key, value) = part.lstrip().split("=", 1)
+            if key == "q":
+                q = float(value)
+            else:
+                media_params.append((key, value))
+        result.append((media_type, tuple(media_params), q))
+    result.sort(comparator)
+    return result
+
+#
+# util.py ends here
